d294afe7de020283b46a936e968d987d2de613cd
Rafa Teivfik <teivfik@gmail.com>
2025-11-09 15:41:40 -0300

    Adicionada verificação de itens

--- models/inventory.gd
+++ models/inventory.gd
@@ -27,30 +27,37 @@ func add_item_by_id(id: int):
 		return
 	add_item(item)
 	
+func has_item(id: int) -> bool:
+	var idx := _find_idx_by_id(id)
+	return idx != -1
+	
 func remove_item(item : Item):
 	var idx = items.find(item)
-	if idx == -1:
-		return
-	items.remove_at(idx)
-	_items_changed()
+	_remove_idx(idx)
 	
 func remove_item_by_id(id: int):
-	var idx := items.find_custom(func(i): return id == i.id)
-	if idx == -1:
-		return
-	items.remove_at(idx)
-	_items_changed()
+	var idx := _find_idx_by_id(id)
+	_remove_idx(idx)
 #endregion
 
 #region Private functions
+func _find_idx_by_id(id:int) -> int:
+	return items.find_custom(func(i): return id == i.id)
+	
 func _items_changed():
-	items_changed.emit()
 	items.sort_custom(func(a,b): return a.id < b.id)
 	if items.is_empty():
 		print("Inventory emptied.")
 	else:
 		var items_print := items.map(func(i): return "(%d: %s)" % [i.id, i.name])
 		print("Current inventory: %s" % items_print)
+	items_changed.emit()
+	
+func _remove_idx(idx:int):
+	if idx == -1:
+		return
+	items.remove_at(idx)
+	_items_changed()
 #endregion
 
 #region Subclasses
