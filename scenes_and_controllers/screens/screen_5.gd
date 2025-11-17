10f08eab39cf63f79babee1da9ea23dc798a83cc
Rafa Teivfik <teivfik@gmail.com>
2025-11-16 22:29:58 -0300

    Tela 5 conectada

---
+++ scenes_and_controllers/screens/screen_5.gd
@@ -0,0 +1,27 @@
+extends ScreenController
+
+#region Overrides
+func _get_interactables_callables() -> Dictionary[String,Callable]:
+	var dict : Dictionary[String,Callable] = {
+		"Drone": func():
+			_get_interactable_by_name("Drone").queue_free()
+			GameManager.player_inventory.add_item_by_id(5),
+		"Terminal": func():
+			if GameManager.player_inventory.has_item(5):
+				_change_screen(3,0,Global.Character.NUZZLE),
+	}
+	return dict
+	
+func _get_trigger_areas_callables() -> Dictionary[int,Callable]:
+	var dict : Dictionary[int,Callable] = {
+		0: func(_body): print("This is the Area 0 callable!")
+	}
+	return dict
+
+func _screen_start(): pass
+	
+func _screen_exit(): pass
+#endregion
+
+#region Private functions
+#endregion
